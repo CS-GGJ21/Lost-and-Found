a904d54329cb1d6ce4fc171496ffb4890ac41087
ausiasxp <72753790+ausiasxp@users.noreply.github.com>
2021-01-31 20:05:38 +0100

    collision and Life bar
    He añadido una barra de vida que disminuye 20 puntos cuando el jugador toca a un enemigo y augmenta cuando el jugador toca un target

---
+++ Assets/Scripts/LifeBar/BarraDeVida.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraDeVida : MonoBehaviour
+{
+    public Image barraDeVida;
+
+    public float vidaActual;
+
+    public float vidaMaxima;
+
+    // Update is called once per frame
+    void Update()
+    {
+        barraDeVida.fillAmount = vidaActual / vidaMaxima;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Enemy")
+        {
+            vidaActual = vidaActual-20;
+        }
+
+        if (collision.gameObject.name == "Target")
+        {
+            vidaActual = vidaActual + 20;
+            Destroy(collision.gameObject);
+        }
+    }
+}
