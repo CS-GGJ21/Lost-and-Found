1c8505b48e85f0c0c171d59aa45dd7ad6daa66ea
ausiasxp <72753790+ausiasxp@users.noreply.github.com>
2021-01-31 17:52:38 +0100

    camera

---
+++ Assets/Scripts/camera/CameraFollow2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour
+{
+    GameObject player;
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+       /* gameObject.transform.position = Vector3(player.transform.position.x,
+                              this.transform.position.y,
+                             player.transform.position.z);*/
+    }
+}
